34f31ce95017356f74038d206b74a9983d48a693
Roger Ruiz <70490580+rogeruiz35@users.noreply.github.com>
2022-05-21 12:10:19 -0400

    First commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() throws Exception{
+        assertTrue( true );
+    }
+}
