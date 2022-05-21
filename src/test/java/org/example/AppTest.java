91d3c367250becf06f1e15d348b1f3233ceec571
Roger Ruiz <roger.ruiz.21@gmail.com>
2022-05-21 12:44:58 -0400

    one more time

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,12 +1,27 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
 
 public class AppTest {
     @Test
-    public void shouldAnswerWithTrue() throws Exception{
-        assertTrue( true );
+    public void test() throws Exception{
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://login.salesforce.com");
+
+        Thread.sleep(8000);
+
+        String pageTitle = driver.getTitle();
+
+        Assert.assertEquals(pageTitle, "Login | Salesforce");
+
+        Thread.sleep(2000);
+
+        driver.quit();
     }
 }
